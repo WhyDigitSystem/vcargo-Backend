6c9f4b930da941af03d9a58c47606161b6bf5ee7
Justin Aravinth <justin@whydigit.in>
2026-01-10 15:58:20 +0530

    corection

--- src/main/java/com/efit/savaari/entity/TripVO.java
+++ src/main/java/com/efit/savaari/entity/TripVO.java
@@ -3,9 +3,20 @@ package com.efit.savaari.entity;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -123,7 +134,7 @@ public class TripVO {
 	private TdriverVO driver;
 
 	@OneToMany(mappedBy = "tripVO", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<TripWaypointVO> waypoints;
+	private List<TripWaypointVO> waypoints = new ArrayList<>();
 
 	
 }
